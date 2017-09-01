bdbfefee1bb8fb156ba5328418f02ec7ba31a189
Jordan Zhang <chrn.zhng@gmail.com>
2017-09-01 09:24:05 -0700

    Daily commit 9/1

--- toy-problems.js
+++ toy-problems.js
@@ -282,4 +282,15 @@ String.prototype.toJadenCase = function () {
   }
   
   descendingOrder(1120)
+
   
+  var decodeMorse = function(morseCode){
+    
+          var a = morseCode.trim().split("   ").map(function(word){
+              x = word.split(" ").map(function(letter){
+                  return MORSE_CODE[letter]
+              }).join("")
+              return x
+          }).join(" ")
+          return a
+    }
