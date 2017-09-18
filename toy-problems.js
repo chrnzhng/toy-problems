ae61b0017f8f20a8427e6c82f54f980a7196e240
Jordan Zhang <chrn.zhng@gmail.com>
2017-09-18 10:29:26 -0700

    added some comments

--- toy-problems.js
+++ toy-problems.js
@@ -407,12 +407,16 @@ function isSquare(n) {
     return Math.sqrt(n) % 1 === 0;
   }
 
+// Find the shortest word in the string input
+
 function findShort(s) {
     shortest = s.split(' ').sort(function (a, b) {
         return a.length - b.length;
     }).shift();
     return shortest.length
 }
+
+// Best Soln.
 
   function findShort(s){
     return Math.min.apply(null, s.split(' ').map(w => w.length));
